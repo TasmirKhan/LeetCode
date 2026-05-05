3f635fd44e30eba0b01006d0ca982b09f23fc36d
Tasmir Khan <masteroffcodes@gmail.com>
2026-05-05 11:15:00 +0530

    Time: 0 ms (100%), Space: 44.4 MB (44.03%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -10,34 +10,40 @@
  */
 class Solution {
 
-    public static int getSize(ListNode head){
-        if(head == null) return 0; 
+    static class Pair{
+        ListNode tail;
+        int count;
+        Pair(int c , ListNode t){
+            count = c;
+            tail = t;
+        }
+    }
+    public static Pair getSize(ListNode head){
+        
         int count = 0 ;
-        ListNode temp = head;
+        ListNode temp = head, tail = null;
         while(temp != null){
             count++;
+            if(temp.next == null){tail = temp;}
             temp = temp.next;
         }
-        return count;
+        return new Pair(count, tail);
     }
     public ListNode rotateRight(ListNode head, int k) {
-        int n = getSize(head);
-        if(k == 0 || head == null || n == 1) return head;
+        Pair p  = getSize(head);
+        ListNode tail = p.tail;
+        int n = p.count;
+        if(n!=0){k = k%n;}
+        if(k == 0 || head == null || n == 1) return head;     
         
-        k = k%n;
-        if(k == 0 ) return head;
-
+        tail.next = head;
         ListNode temp = head;
         for(int i = 1 ; i<n - k ; i++){
             temp = temp.next;
         }
+
         ListNode head2 = temp.next;
-        ListNode newStart = temp.next;
         temp.next = null;
-        while(newStart != null){
-            if(newStart.next == null){newStart.next = head; break;}
-            newStart = newStart.next;
-        }
         
         return head2;
 
