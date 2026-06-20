324cdd486f8948ee9c31a4990be007f8459a9c26
Tasmir Khan <masteroffcodes@gmail.com>
2026-06-20 21:06:47 +0530

    Time: 0 ms (100%), Space: 43.7 MB (68.66%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,18 +1,31 @@
 class Solution {
+
+    public static void revArr(int[]arr){
+       int n = arr.length, low = 0 , high = n-1;
+        while(low<=high){
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         int[][] dup = new int[n][n];
 
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j<n ; j++){
-                dup[i][j] = matrix[n-1-j][i];
+            for(int j = 0 ; j<i ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         } 
         for(int i = 0 ; i<n ; i++){
-            for(int j = 0 ; j < n ; j++){
-                matrix[i][j] = dup[i][j]; 
-            }
+           revArr(matrix[i]);
         }
+
+
         // matrix = dup; why can't we achieve through it.
     }
 }
