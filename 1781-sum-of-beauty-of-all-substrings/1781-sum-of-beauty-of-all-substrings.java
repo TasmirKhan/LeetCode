5aeaa26f1dd8b40a06a4738756da6c6989b485dc
Tasmir Khan <masteroffcodes@gmail.com>
2026-06-25 11:26:36 +0530

    Time: 503 ms (26.58%), Space: 46.6 MB (21.8%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,27 +1,40 @@
+import java.util.*;
+
 class Solution {
     public int beautySum(String s) {
         int n = s.length();
-        
         int sum = 0;
-        
-        for(int i = 0 ; i<n ; i++){
-            for(int j = i ; j<n ; j++){
-                int [] arr = new int[26];
-                int max = 0, min = Integer.MAX_VALUE;
-                for(int k = i ; k<=j ; k++){
-                    arr[s.charAt(k) - 'a']++;
-                }
-                for(int x : arr){
-                    if(x>0){
-                        max = Math.max(max,x);
-                        min = Math.min(min,x);
-                    }
+
+        // Loop through all substrings
+        for (int i = 0; i < n; i++) {
+            Map<Character, Integer> freq = new HashMap<>();
+
+            for (int j = i; j < n; j++) {
+                // Increase character frequency
+                freq.put(s.charAt(j), freq.getOrDefault(s.charAt(j), 0) + 1);
+
+                int maxi = Integer.MIN_VALUE;
+                int mini = Integer.MAX_VALUE;
+
+                // Calculate max and min frequency
+                for (int val : freq.values()) {
+                    mini = Math.min(mini, val);
+                    maxi = Math.max(maxi, val);
                 }
 
-                 sum += (max-min);
+                // Add to sum
+                sum += (maxi - mini);
             }
-           
         }
+
         return sum;
     }
 }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         Solution sol = new Solution();
+//         String s = "xyx";
+//         System.out.println("Beauty Sum: " + sol.beautySum(s));
+//     }
+// }
