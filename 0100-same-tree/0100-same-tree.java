aba5950358aba63e0df1eaed15b8f25e5a7b219a
Tasmir Khan <masteroffcodes@gmail.com>
2026-07-30 15:30:09 +0530

    Time: 0 ms (100%), Space: 42.6 MB (94.06%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,13 +15,13 @@
  */
 class Solution {
     public static boolean helper(TreeNode p , TreeNode q){
-        if(p == null && q == null) return true;
-        if(p== null || q == null ) return false;
-        if(p.val != q.val) return false;
-        return helper(p.left,q.left) && helper(p.right, q.right);
+       if(p==null && q == null){ return true;}
+       if(p==null || q == null){ return false;}
+       if(p.val != q.val) return false;
+       return helper(p.left, q.left) && helper(p.right, q.right);
 
     }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return helper(p,q);
+        return  helper(p,q);
     }
 }
