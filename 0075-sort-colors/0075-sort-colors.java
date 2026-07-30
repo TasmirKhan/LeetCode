39fb97906dae7b32188ddcab59136ffe0c3a0e41
Tasmir Khan <masteroffcodes@gmail.com>
2026-07-30 11:45:35 +0530

    Time: 0 ms (100%), Space: 43.2 MB (89.91%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int zero =0, one = 0 , two = 0;
+        int n = nums.length;
+        for(int num : nums){
+            if(num == 0){ zero++;}
+            else if(num == 1){one++;}
+            else two++;
+        }
+        int i = 0;
+        while(zero >0 && i<n){
+            nums[i] = 0;
+            i++;
+            zero--;
+        }
+        while(one >0 && i < n){
+            nums[i] = 1;
+            i++;
+            one--;
+        }
+        while(two >0 && i < n){
+            nums[i] = 2;
+            i++;
+            two--;
+        }
+    }
+}
