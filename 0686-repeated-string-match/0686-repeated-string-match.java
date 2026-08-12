38448ce770785f60338d848405e3bb9bede5abb5
Tasmir Khan <masteroffcodes@gmail.com>
2026-08-12 11:08:15 +0530

    Time: 5 ms (98.09%), Space: 43.5 MB (71.12%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -1,107 +1,60 @@
 class Solution {
-
     public int repeatedStringMatch(String a, String b) {
-
         int alen = a.length();
         int blen = b.length();
-
-        int minCount = (blen + alen - 1) / alen;
-
+        int minCount=(blen + alen - 1)/alen;
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < minCount; i++) {
+        for(int i = 0 ; i<minCount ; i++){
             sb.append(a);
         }
 
-        if (rabinKarp(sb.toString(), b)) {
+        if(rabinKarp(sb.toString(), b)){
             return minCount;
         }
-
         sb.append(a);
 
-        if (rabinKarp(sb.toString(), b)) {
-            return minCount + 1;
+        if(rabinKarp(sb.toString(),b)){
+            return minCount+1;
         }
-
         return -1;
     }
 
-    private boolean rabinKarp(String text, String pattern) {
-
+    private boolean rabinKarp(String text , String pattern){
         int n = text.length();
         int m = pattern.length();
 
-        if (m > n) {
-            return false;
-        }
+        if(m>n){ return false; }
 
         long base = 31;
-        long mod = 1_000_000_007;
+        long mod = 1000000000 + 7 ;
 
-        long patternHash = 0;
+        long patternHash = 0 ;
         long windowHash = 0;
         long power = 1;
 
-        // base^(m-1)
-        for (int i = 0; i < m - 1; i++) {
-            power = (power * base) % mod;
+        //base^(m-1)
+        for(int i = 0 ; i < m-1 ; i++){
+            power = (power*base)%mod;
         }
 
-        // Calculate initial hashes
-        for (int i = 0; i < m; i++) {
+        for(int i = 0 ; i<m ; i++){
+            patternHash = (patternHash*base + pattern.charAt(i))%mod;
+            windowHash = (windowHash*base + text.charAt(i))%mod;
 
-            patternHash =
-                (patternHash * base + pattern.charAt(i)) % mod;
-
-            windowHash =
-                (windowHash * base + text.charAt(i)) % mod;
         }
 
-        // Check first window
-        if (patternHash == windowHash &&
-            text.regionMatches(0, pattern, 0, m)) {
+        if(patternHash == windowHash && text.regionMatches(0,pattern,0,m)){
             return true;
         }
 
-        // Rolling hash
-        for (int i = m; i < n; i++) {
+        for(int i = m ; i<n ; i++){
+            windowHash = (windowHash - text.charAt(i-m)*power%mod + mod)%mod;
+            windowHash = (windowHash*base + text.charAt(i))%mod;
 
-            // Remove leftmost character
-            windowHash =
-                (windowHash -
-                text.charAt(i - m) * power % mod + mod) % mod;
-
-            // Add new character
-            windowHash =
-                (windowHash * base + text.charAt(i)) % mod;
-
-            int start = i - m + 1;
-
-            if (windowHash == patternHash &&
-                text.regionMatches(start, pattern, 0, m)) {
-                return true;
-            }
+            int start = i-m+1;
+            if(windowHash == patternHash && text.regionMatches(start, pattern , 0, m)){ return true;}
         }
-
         return false;
     }
 }
-
-// class Solution {
-//     public int repeatedStringMatch(String a, String b) {
-//          int blen = b.length();
-//          int alen = a.length();
-
-//         //  if(alen > blen){if(a.contains(b)){ return 1;}}
-//          String temp = "";
-//          int cnt = 0;
-//          while(temp.length() < 2*b.length()){
-//             temp = temp+a;
-//             cnt++;
-//             if(temp.length() >= blen){
-//                 if(temp.contains(b))return cnt;
-//             }
-//          }
-//          return -1;
-//     }
-// }
