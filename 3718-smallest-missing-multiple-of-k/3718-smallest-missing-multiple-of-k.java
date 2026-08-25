f56f978b5c7b5902c6bda550c992db07bbe691c7
Tasmir Khan <masteroffcodes@gmail.com>
2026-08-25 11:32:48 +0530

    Time: 1 ms (82%), Space: 44.9 MB (83.31%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -1,11 +1,22 @@
 class Solution {
     public int missingMultiple(int[] nums, int k) {
-        Arrays.sort(nums);
-        int x  = k;
-        for(int num : nums){
-            if(num%k == 0 && num > x) return x;
-            if(num%k ==0 && num == x) x += k;
+        boolean[] arr = new boolean[100];
+        int i = 0;
+        for(int idx : nums){
+            arr[idx-1] = true;
         }
-        return x;
+        for( i = k-1;i<arr.length;i+=k){
+            if(arr[i] == false) return i+1;
+        }
+        return i+1;
+
+
+        // Arrays.sort(nums);
+        // int x  = k;
+        // for(int num : nums){
+        //     if(num%k == 0 && num > x) return x;
+        //     if(num%k ==0 && num == x) x += k;
+        // }
+        // return x;
     }
 }
