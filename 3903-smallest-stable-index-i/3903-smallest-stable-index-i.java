5e755b4c630f1acace734a6a5306e454a32c32bc
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-04 19:59:49 +0530

    Time: 1 ms (99.79%), Space: 46.3 MB (70.85%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -1,18 +1,31 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
-        int max = Integer.MIN_VALUE;
-        
-        for(int i = 0 ; i<nums.length ; i++){
-            max = Math.max(max, nums[i]);
-            int min = Integer.MAX_VALUE;
-            for(int j = i ; j<nums.length ; j++){
-                if(nums[j] < min){
-                 min = nums[j];
-                }                
-            }
-            int stb = max-min;
-            if(stb <=k) return i;
+        // int max = Integer.MIN_VALUE;
+        int n = nums.length;
+        int[] suffixMin = new int[n];
+        suffixMin[n-1] = nums[n-1];
+        for(int i = n-2 ; i>=0 ; i--){
+            suffixMin[i] = Math.min(suffixMin[i+1], nums[i]);
         }
+
+        int max = Integer.MIN_VALUE;
+        for(int i = 0 ; i<nums.length ; i++){
+            max = Math.max(nums[i],max);
+            if(max - suffixMin[i] <= k) return i;
+        }
+
         return -1;
+        // for(int i = 0 ; i<nums.length ; i++){
+        //     max = Math.max(max, nums[i]);
+        //     int min = Integer.MAX_VALUE;
+        //     for(int j = i ; j<nums.length ; j++){
+        //         if(nums[j] < min){
+        //          min = nums[j];
+        //         }                
+        //     }
+        //     int stb = max-min;
+        //     if(stb <=k) return i;
+        // }
+       // return -1;
     }
 }
