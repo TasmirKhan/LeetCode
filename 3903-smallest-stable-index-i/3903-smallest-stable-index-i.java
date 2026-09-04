a352aa2102e94239111352523b841b303ef411f8
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-04 20:02:40 +0530

    Time: 1 ms (99.79%), Space: 46.2 MB (70.85%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.java
@@ -2,14 +2,15 @@ class Solution {
     public int firstStableIndex(int[] nums, int k) {
        
         int n = nums.length;
-        
+
+        // 1. Generate suffixMin from last of the nums array.
         int[] suffixMin = new int[n];
         suffixMin[n-1] = nums[n-1];
-
         for(int i = n-2 ; i>=0 ; i--){
             suffixMin[i] = Math.min(suffixMin[i+1], nums[i]);
         }
 
+        // 2. Subtract max with suffixMin element to compare the instability.
         int max = Integer.MIN_VALUE;
         for(int i = 0 ; i<nums.length ; i++){
             max = Math.max(nums[i],max);
