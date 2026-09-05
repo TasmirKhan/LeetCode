62e413bafe68f7f5aa8d53a70a65e564175bdbd8
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-05 19:36:42 +0530

    Time: 4 ms (88.55%), Space: 133.3 MB (14.89%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,12 +1,15 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
+        
+        // Step 1. Create SuffixMin Array
         int[] suffixMin = new int[n];
         suffixMin[n-1] = nums[n-1];
         for(int i = n-2 ; i>=0 ; i--){
             suffixMin[i] = Math.min(nums[i], suffixMin[i+1]);
         }
 
+        // Step 2. Get the answer by subtracting the current min value from suffixMin(Temp array ) to the Current max value from the Nums(main array).
         int max = Integer.MIN_VALUE;
         for(int i = 0 ; i<n; i++){
             max = Math.max(max,nums[i]);
