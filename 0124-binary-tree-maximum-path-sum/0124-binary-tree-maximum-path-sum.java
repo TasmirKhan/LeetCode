38cdfbe2f79d4d0ccf790f750c1341c75ffa5c31
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-05 20:33:42 +0530

    Time: 0 ms (100%), Space: 46.7 MB (56.63%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -15,25 +15,34 @@
  */
 class Solution {
     static int maxSum = Integer.MIN_VALUE;
+    public static int dfs(TreeNode root){
+        if(root == null) return 0;
+        int left = Math.max(0, dfs(root.left));
+        int right = Math.max(0,dfs(root.right));
+
+        maxSum = Math.max(maxSum, left+right+root.val);
+        return root.val + Math.max(left, right);
+    }
     public int maxPathSum(TreeNode root) {
-        maxSum = Integer.MIN_VALUE;
-        maxGain(root);
+        maxSum = Integer.MIN_VALUE; //Reseting the Static values.
+        dfs(root);
+        // maxGain(root);
         return maxSum;
     }
 
-    static void maxGain(TreeNode root){
-        if(root == null) return ;
-        int left = tillSum(root.left);
-        int right = tillSum(root.right);
-        maxSum = Math.max(maxSum, left+right+root.val);
-        maxGain(root.left);
-        maxGain(root.right);
-    }
+    // static void maxGain(TreeNode root){
+    //     if(root == null) return ;
+    //     int left = tillSum(root.left);
+    //     int right = tillSum(root.right);
+    //     maxSum = Math.max(maxSum, left+right+root.val);
+    //     maxGain(root.left);
+    //     maxGain(root.right);
+    // }
 
-    static int tillSum(TreeNode root){
-        if(root == null) return 0;
-        int left = tillSum(root.left);
-        int right = tillSum(root.right);
-        return Math.max(0, root.val + Math.max(left,right));
-    }
+    // static int tillSum(TreeNode root){
+    //     if(root == null) return 0;
+    //     int left = tillSum(root.left);
+    //     int right = tillSum(root.right);
+    //     return Math.max(0, root.val + Math.max(left,right));
+    //}
 }
