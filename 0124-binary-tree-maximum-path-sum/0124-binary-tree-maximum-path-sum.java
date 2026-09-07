ddb2284b09f6e7bc17a820267130eefc38014faf
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-07 14:44:50 +0530

    Time: 1 ms (31.87%), Space: 46.4 MB (94.83%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -15,39 +15,17 @@
  */
 class Solution {
     static int maxSum = Integer.MIN_VALUE;
-
-    // Optimal DFS Approach 
     public static int dfs(TreeNode root){
         if(root == null) return 0;
-        int left = Math.max(0, dfs(root.left)); // using max to avoid -ve values.
+        int left = Math.max(0,dfs(root.left));
         int right = Math.max(0,dfs(root.right));
+        maxSum  = Math.max(maxSum, left+right+root.val);
 
-        maxSum = Math.max(maxSum, left+right+root.val);
-        return root.val + Math.max(left, right);
+        return Math.max(left,right) + root.val;
     }
     public int maxPathSum(TreeNode root) {
-        maxSum = Integer.MIN_VALUE; //Reseting the Static values.
+        maxSum = Integer.MIN_VALUE;
         dfs(root);
-        // maxGain(root); Brute Approach Call (ignore it):
         return maxSum;
     }
-
-
-
-    // Brute Force Approach.
-    // static void maxGain(TreeNode root){
-    //     if(root == null) return ;
-    //     int left = tillSum(root.left);
-    //     int right = tillSum(root.right);
-    //     maxSum = Math.max(maxSum, left+right+root.val);
-    //     maxGain(root.left);
-    //     maxGain(root.right);
-    // }
-
-    // static int tillSum(TreeNode root){
-    //     if(root == null) return 0;
-    //     int left = tillSum(root.left);
-    //     int right = tillSum(root.right);
-    //     return Math.max(0, root.val + Math.max(left,right));
-    //}
 }
