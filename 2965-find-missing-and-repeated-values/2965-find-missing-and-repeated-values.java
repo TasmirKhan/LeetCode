5d8cf9edf8179b3fcd2f9445733fcf656370ad92
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-14 19:47:39 +0530

    Time: 1 ms (99.28%), Space: 47.6 MB (24.47%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,20 +1,43 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        HashMap<Integer,Integer> set = new HashMap<>();
+        int m = grid.length;
+        int n = m*m;
+
+        int[] freq = new int[n+1];
+
+        int missing = -1;
+        int repeating = -1;
         int sum = 0;
-        int n = grid.length * grid.length;
         int nsum = (n*(n+1))/2;
-        for(int i = 0 ; i<grid.length ; i++){
-            for(int j = 0 ; j<grid[0].length; j++){
-                set.put(grid[i][j], set.getOrDefault(grid[i][j], 0)+1);
-                sum += grid[i][j];
+
+        for(int[] x : grid ){
+            for(int y : x){
+                freq[y]++;
+                if(freq[y] == 2){ repeating = y;}
+                sum += y;
             }
         }
-        int repeating = 0;
-        for(int x  : set.keySet()){
-            if(set.get(x) >= 2){ repeating = x; break;}
-        }
-        int missing = nsum - sum + repeating;
+
+        missing = nsum - sum + repeating;
+
         return new int[] {repeating,missing};
+
+
+        // HashMap<Integer,Integer> set = new HashMap<>();
+        // int sum = 0;
+        // int n = grid.length * grid.length;
+        // int nsum = (n*(n+1))/2;
+        // for(int i = 0 ; i<grid.length ; i++){
+        //     for(int j = 0 ; j<grid[0].length; j++){
+        //         set.put(grid[i][j], set.getOrDefault(grid[i][j], 0)+1);
+        //         sum += grid[i][j];
+        //     }
+        // }
+        // int repeating = 0;
+        // for(int x  : set.keySet()){
+        //     if(set.get(x) >= 2){ repeating = x; break;}
+        // }
+        // int missing = nsum - sum + repeating;
+        // return new int[] {repeating,missing};
     }
 }
