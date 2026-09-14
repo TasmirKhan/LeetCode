da8dc6491d32a6b939cabca3aec83449d1ea6ae2
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-14 22:29:33 +0530

    Time: 2 ms (45.16%), Space: 94.5 MB (32.99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,19 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        Stack<Integer> st = new Stack<>();
-        int n = prices.length - 1;
-        int res  = 0;
-        st.push(prices[n--]);
-        while(n >= 0){
-            if(prices[n] > st.peek()){
-            while(!st.isEmpty() && prices[n] > st.peek()){
-                st.pop();
+       int j = 0;
+       int n = prices.length;
+       int res = Integer.MIN_VALUE;
+       int min = prices[j++];
+       int max = 0;
+       while(j<n){
+            if(prices[j] < min){
+                min = prices[j];
             }
-            st.push(prices[n]); }
-
-            res = Math.max(res, st.peek() - prices[n]);
-            n--;
-        }
-    return res;
+            max = Math.max(max, prices[j] - min );
+            j++;
+       }
+       return max;
     }
 }
