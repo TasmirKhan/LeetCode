b4f911b05791d56873bcf24616d42817a6ab84cd
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-16 12:43:37 +0530

    Time: 2 ms (95.73%), Space: 48.7 MB (30.5%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -2,8 +2,10 @@ class Solution {
     private int BitCounter(int n){
         int x = 0;
         while(n>0){
-            if((n&1) == 1) x++;
-            n = n>>1;
+            n = n& n-1;
+            x++;
+            // if((n&1) == 1) x++;
+            // n = n>>1;
         }
         return x;
     }
