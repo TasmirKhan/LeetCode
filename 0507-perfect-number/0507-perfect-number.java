a7803768c8ddedc0e7d3775590f89ac25bdbd6d6
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-17 17:41:42 +0530

    Time: 1 ms (96.37%), Space: 42.4 MB (10.03%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,14 +1,15 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
+        if(num == 1) return false;
         int sum = 0;
         int x = num;
         for(int i = 1 ; i*i <= num ; i++){
             if(num%i == 0){
                 sum += i;
-                if(i != num/i){ sum += num/i; }
+                if(i != 1  && i != num/i){ sum += num/i; }
             }
         }
-        sum -= num;
+        
         return sum == x;
     }
 }
