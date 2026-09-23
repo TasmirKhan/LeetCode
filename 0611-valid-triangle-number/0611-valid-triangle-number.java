b1d8bceed7fceb36430b890df06ce763d8c00685
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-23 13:42:36 +0530

    Time: 27 ms (87.97%), Space: 45.8 MB (39.94%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -4,16 +4,30 @@ class Solution {
         int count = 0;
 
         int n = nums.length - 1;
-        while(n>=2){
-            int thirdSide = nums[n];
-            for(int i = 0; i<=n-2 ; i++){
-                for(int j = i+1 ; j<=n-1 ; j++){
-                    if(nums[i] + nums[j] > thirdSide){
-                        count++;
-                    }
+        for(int i = n; i>=2 ; i--){
+            int thirdSide = nums[i];
+            int k = 0 ; 
+            int j = i-1;
+            while(k < j ){
+                if(nums[k] + nums[j] > thirdSide){
+                    count += j-k;
+                    j--;
                 }
+
+                else {
+                    k++;
+                }
+                
             }
-            n-=1;
+
+            // for(int i = 0; i<=n-2 ; i++){
+            //     for(int j = i+1 ; j<=n-1 ; j++){
+            //         if(nums[i] + nums[j] > thirdSide){
+            //             count++;
+            //         }
+            //     }
+            // }
+            
         }
         return count;
     }
