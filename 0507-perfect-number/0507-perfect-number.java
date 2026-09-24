257779cb4036b16ca3720985e4db4460be9d1be5
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-24 14:04:53 +0530

    Time: 2 ms (85.74%), Space: 41.9 MB (89.82%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -3,6 +3,7 @@ class Solution {
         if(num == 1) return false;
         int sum = 0;
         int x = num;
+        
         for(int i = 1 ; i*i <= num ; i++){
             if(num%i == 0){
                 sum += i;
