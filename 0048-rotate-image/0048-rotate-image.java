0a1a7a0658aafe96d97272592276aa0ee5a2ccf9
Tasmir Khan <masteroffcodes@gmail.com>
2026-09-26 10:25:15 +0530

    Time: 94 ms (1.36%), Space: 43.8 MB (52.67%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,31 +1,34 @@
 class Solution {
-
-    public static void revArr(int[]arr){
-       int n = arr.length, low = 0 , high = n-1;
-        while(low<=high){
-            int temp = arr[low];
-            arr[low] = arr[high];
-            arr[high] = temp;
-            low++;
-            high--;
-        }
-    }
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        int[][] dup = new int[n][n];
-
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j<i ; j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
-            }
-        } 
+    private int[][] transpose(int[][] arr){
+        int n = arr.length;
         for(int i = 0 ; i<n ; i++){
-           revArr(matrix[i]);
+            for(int j = 0; j<i; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        return arr;
         }
 
-
-        // matrix = dup; why can't we achieve through it.
+        private int[][] reverse(int[][] arr){
+            int n = arr.length;
+            for(int i = 0 ; i<n ; i++){
+                int low = 0 , high = n-1;
+                while(low < high){
+                    int temp = arr[i][low];
+                    arr[i][low] = arr[i][high];
+                    arr[i][high] = temp;
+                    low++; high--;
+                }
+            }
+            return arr;
+        }
+    
+    public void rotate(int[][] matrix) {
+        Solution obj = new Solution();
+        matrix = obj.transpose(matrix);
+        matrix = obj.reverse(matrix);
+        
     }
 }
